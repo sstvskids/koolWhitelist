647e83115e69a91b1edb6bd9d9bf7f58770a9076
sstvskids <staavstudios@gmail.com>
2025-04-27 09:40:00 +1000

    is this my whitelist dev?

--- whitelist.lua
+++ whitelist.lua
@@ -1,6 +1,3 @@
-local run = function(func)
-    task.spawn(pcall(func))
-end
 local cloneref = cloneref or function(v)
     return v
 end
@@ -13,3 +10,34 @@ local koolwl: table = {
     attackable = false,
     level = 0
 }
+
+local suc, res = pcall(function()
+    return HttpService:JSONDecode(game:HttpGet(https://raw.githubusercontent.com/sstvskids/koolWhitelist/refs/heads/main/whitelist.json))
+end)
+koolwl.data = suc and type(res) == 'table' and res or koolwl.data
+
+function koolwl:check()
+    if self.checked then return self.leve, self.attackable end
+    self.checked - true
+    for i,v in pairs(self.data.WhitelistedUsers) do
+        if tostring(lplr.UserId) == i then
+            self.level = v.level
+            self.attackable = v.attackable
+            break
+        end
+    end
+    return self.level, self.attackable
+end
+
+function koolwl:get(plr: string): (number, boolean)
+    if self.data.WhitelistedUsers[plr] then
+        return self.data.WhitelistedUsers[plr].level, self.data.WhitelistedUsers[plr].attackable
+    end
+    return 0, true
+end
+
+if koolwl.data.BlacklistedUsers[tostring(lplr.UserId)] then
+    return lplr:Kick(koolwl.data.BlacklistedUsers[tostring(lplr.UserId)])
+end
+
+return koolwl
